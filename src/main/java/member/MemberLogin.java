e65dbbd35007de2a4d5eee41409bfb1e82760704
alstjd974 <alstjd974@nate.com>
2023-08-17 17:15:16 +0900

    Login Project

---
+++ src/main/java/member/MemberLogin.java
@@ -0,0 +1,37 @@
+package member;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import utility.ParameterUtil;
+
+@WebServlet("/member/login")
+public class MemberLogin extends HttpServlet {
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// 파라미터 검증
+		String id = ParameterUtil.getString(request, "id");
+		String pw = ParameterUtil.getString(request, "pw");
+		
+		if(!ParameterUtil.isId(id)) {
+			response.setStatus(400);
+			return;
+		} else if(!ParameterUtil.isPw(pw)){
+			return;
+		}
+		
+		MemberDto member = new MemberDto(id, pw, null, null);
+		
+		MemberService service = new MemberService();
+		boolean result = service.correctIdPw(member);
+		
+		if(!result) {
+			response.setStatus(400);
+		}
+	}
+}
--- src/main/java/member/MemberService.java
+++ src/main/java/member/MemberService.java
@@ -3,22 +3,43 @@ package member;
 public class MemberService {
 	public String join(MemberDto member) {
 		MemberDao dao = new MemberDao();
-		
+
 		// 아이디 중복 여부 확인
-		if(dao.selectOneById(member.getId()) != null) {
+		if (dao.selectOneById(member.getId()) != null) {
 			return "duplicate_id";
 		}
-		
+
 		// 연락처 중복 여부 확인
-		if(dao.selectOneByTel(member.getTel()) != null) {
+		if (dao.selectOneByTel(member.getTel()) != null) {
 			return "duplicate_tel";
 		}
-		
+
 		// 회원가입
-		if(dao.insertByMemberDto(member) == 1) {
+		if (dao.insertByMemberDto(member) == 1) {
 			return "OK";
 		} else {
 			return "unknown error";
+		}
+	}
+
+	public boolean correctIdPw(MemberDto member) {
+		MemberDao dao = new MemberDao();
+
+		MemberDto selectedMember = dao.selectOneById(member.getId());
+		if (selectedMember == null) {
+			return false;
+		}
+
+		// 사용자가 입력한 비밀번호
+		String userInputPw = member.getPw();
+
+		// 사용자가 입력한 아이디로 조회한 회원 정보의 빌밀번호
+		String storedUserPw = selectedMember.getPw();
+
+		if (!userInputPw.equals(storedUserPw)) {
+			return false;
+		} else {
+			return true;
 		}
 	}
 }
